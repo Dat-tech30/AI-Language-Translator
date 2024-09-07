a3986f3c7567f307a20c370d3a2895f06733389c
Dat <phandinhd@outlook.com>
2024-09-06 19:36:57 -0700

    added code from hugging ace for Llama 3.1 8B and commented out previous work for reference/learning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 # pip install transformers ipywidgets gradio --upgrade
 
-import gradio as gr # UI Library
+"""import gradio as gr # UI Library
 from transformers import pipeline # Transformers Pipeline
 
 # Loading up the pipeline
@@ -19,5 +19,30 @@ interface = gr.Interface(fn=translate_transformers,
                          inputs=gr.inputs.Textbox(lines=2, placeholder='Text to translate'),
                         outputs='text')
 
-interface.launch()
+interface.launch()"""
+
+
+
+import transformers
+import torch
+
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+)
+
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
 
